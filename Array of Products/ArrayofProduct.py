1150b6cadf27ff59fada723bec22ec0d6f185be3
Ivan Trejo <ivantrejo@IvanTrejo.localdomain>
2022-06-12 10:10:21 -0700

    added question and solution to Array of Product

---
+++ Array of Products/ArrayofProduct.py
@@ -0,0 +1,27 @@
+def arrayOfProducts(array):
+  # exempt is the variable that will be exempt from the compounding of integers
+  exempt = 0
+  # answer will hold all values
+  answer = []
+
+  while exempt < len(array):
+    #runnning sum will 'refresh with each while loop'
+    runningSum = 1
+      
+    for idx, i in enumerate(array):
+      #if the idx of the loop is the exempt, continue to the next loop
+      if idx == exempt:
+          continue
+      #begin compounding the values
+      else:
+          runningSum *= i
+    
+    #add the values to the answer array and also increase the exempted value
+    answer.append(runningSum)
+    exempt += 1
+
+  return answer
+
+#BIG O EVal
+#Time = O(n2)
+#space = O(n)
---
+++ Array of Products/ArrrayOfProdREADME.md
@@ -0,0 +1,16 @@
+# Array Of Products
+
+- Write a function that takes in a non-empty array of integers and returns an array of the same lengthe, where each element in the output array is equal to the product of every other number in the input array. 
+
+- in other words, the value of output[i] is equal to the product of every number in the input array other than input[i].
+
+- Note that you're expected to solve the problem without using division
+
+- EX: Input
+```
+array = [5, 1, 4, 2]
+```
+- EX: Output
+```
+[8, 40, 10, 20]
+```
